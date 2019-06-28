7fef1e1a58d08b3aeb1c2fac706dfbb100d5b8c6
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2019-06-28 12:31:36 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    l_d = pypandoc.convert('README.md')
+except:
+    l_d = ''
+
+# Get the version.
+version = {}
+with open("version.py") as fp:
+    exec(fp.read(), version)
+setup(
+    name='observations',
+    version=version['__version__'],
+    description='Observations module by Artesia',
+    long_description=l_d,
+    url='https://artesia-water.nl',
+    author='Artesia',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    platforms='Windows, Mac OS-X',
+    install_requires=['numpy>=1.15', 'matplotlib>=3.0',
+                      'pandas>=0.24', 'scipy>=1.2', ],
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+)
