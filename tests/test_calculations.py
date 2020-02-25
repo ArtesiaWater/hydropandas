7aac88515a5f06c2264110cc260e057b9d43e703
OnnoEbbens <onnoebbens@gmail.com>
2020-02-25 13:53:43 +0100

    update and add test functions

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -6,6 +6,8 @@ Created on Fri Nov  8 11:48:29 2019
 """
 
 import test_to_from as ttf
+import flopy
+import numpy as np
 
 
 def test_within_extent():
@@ -42,18 +44,43 @@ def test_obscollection_get_max():
     omax = gw.stats.get_max()
     return omax
 
-def test_get_filter_num():
+#%%
+def test_set_filter_num():
     dino_gw = ttf.test_obscollection_dinozip_gw()
-    dino_gw.gwobs.get_filter_num(if_exists='replace')
+    dino_gw.gwobs.set_filter_num(if_exists='replace')
     return dino_gw
 
 
-def test_get_filter_num_location():
+def test_set_filter_num_location():
     fews_gw_prod = ttf.test_obscollection_fews()
     fews_gw_prod.add_meta_to_df('locationId')
-    fews_gw_prod.gwobs.get_filter_num_location('locationId',
+    fews_gw_prod.gwobs.set_filter_num_location('locationId',
                                                if_exists='replace')
     return fews_gw_prod
+
+def test_get_modellayers():
+    modelname = 'tutorial1'
+    ml = flopy.modflow.Modflow(modelname, exe_name='mf2005')
+    # Model domain and grid definition
+    Lx = 1000000.
+    Ly = 1000000.
+    ztop = 50.
+    zbot = -150.
+    nlay = 4
+    nrow = 10
+    ncol = 10
+    delr = Lx / ncol
+    delc = Ly / nrow
+    delv = (ztop - zbot) / nlay
+    botm = np.linspace(ztop, zbot, nlay + 1)
+    # Create the discretization object
+    dis = flopy.modflow.ModflowDis(ml, nlay, nrow, ncol, delr=delr, delc=delc,
+                                   top=ztop, botm=botm[1:])
+    
+    dino_gw = ttf.test_obscollection_dinozip_gw()
+    modellayers = dino_gw.gwobs.get_modellayers(ml)
+    
+    return modellayers
 
 
 def test_get_nearest_point():
