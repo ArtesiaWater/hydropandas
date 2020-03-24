89d383f5f83667b20ee5c22b70521b1007b00a78
OnnoEbbens <onnoebbens@gmail.com>
2020-03-24 12:04:33 +0100

    move tests to gwobs tests

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -50,48 +50,11 @@ def test_obscollection_get_max():
     return omax
 
 #%%
-def test_set_filter_num():
-    dino_gw = ttf.test_obscollection_dinozip_gw()
-    dino_gw.gwobs.set_filter_num(if_exists='replace')
-    return dino_gw
-
-def test_set_filter_num_pystore():
-    obsc = ttf.test_obscollection_from_pystore()
-    obsc.gwobs.set_filter_num(if_exists='replace')
-    return obsc
-
-def test_set_filter_num_location():
-    fews_gw_prod = ttf.test_obscollection_fews_lowmemory()
-    fews_gw_prod.gwobs.set_filter_num_location('locatie',
-                                               if_exists='replace')
-    return fews_gw_prod
-
-def test_get_modellayers():
-    modelname = 'tutorial1'
-    ml = flopy.modflow.Modflow(modelname, exe_name='mf2005')
-    # Model domain and grid definition
-    Lx = 1000000.
-    Ly = 1000000.
-    ztop = 50.
-    zbot = -150.
-    nlay = 4
-    nrow = 10
-    ncol = 10
-    delr = Lx / ncol
-    delc = Ly / nrow
-    delv = (ztop - zbot) / nlay
-    botm = np.linspace(ztop, zbot, nlay + 1)
-    # Create the discretization object
-    dis = flopy.modflow.ModflowDis(ml, nlay, nrow, ncol, delr=delr, delc=delc,
-                                   top=ztop, botm=botm[1:])
-
-    dino_gw = ttf.test_obscollection_dinozip_gw()
-    modellayers = dino_gw.gwobs.get_modellayers(ml)
-
-    return modellayers
 
 
 def test_get_nearest_point():
+    # check two of the same observation collections
+    # every point must find itself as the nearest point
     dino_gw = ttf.test_obscollection_dinozip_gw()
     fl = ttf.test_obscollection_fieldlogger()
     dino_gw[['nearest point', 'distance nearest point']
