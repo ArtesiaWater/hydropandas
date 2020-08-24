13bd61c237d965f190e6e5afe763a5b9d877e5a6
OnnoEbbens <onnoebbens@gmail.com>
2020-08-24 16:03:00 +0200

    import path dependecy

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
