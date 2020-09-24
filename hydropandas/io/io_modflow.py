fda79c2a4c6a05577afa7a5aaaed2f0f9537c0c2
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2020-09-24 18:19:42 +0200

    improve getting obs from modflow model

--- hydropandas/io/io_modflow.py
+++ hydropandas/io/io_modflow.py
@@ -84,7 +84,7 @@ def read_imod_results(obs_collection, ml, runfile, mtime, model_ws,
 
 
 def read_modflow_results(obs_collection, ml, hds_arr, mtime,
-                         modelname='', nlay=None, exclude_layers=0,
+                         modelname='', nlay=None, exclude_layers=None,
                          verbose=False):
     """Read modflow groundwater heads at points in obs_collection.
 
@@ -102,15 +102,15 @@ def read_modflow_results(obs_collection, ml, hds_arr, mtime,
         modelname
     nlay : int, optional
         number of layers if None the number of layers from ml is used.
-    exclude_layers : int, optional
-        exclude the observations up to these modellayers
+    exclude_layers : list of int, optional
+        exclude the observations in these modellayers
     verbose : boolean, optional
         Print additional information to the screen (default is False).
     """
 
     if ml.modelgrid.xoffset == 0 or ml.modelgrid.yoffset == 0:
         warnings.warn('you probably want to set the xll '
-                      'and/or yll attributes of dis.sr')
+                      'and/or yll attributes in DIS!')
 
     if nlay is None:
         nlay = ml.modelgrid.nlay
@@ -125,17 +125,20 @@ def read_modflow_results(obs_collection, ml, hds_arr, mtime,
     vtx, wts = util.interp_weights(xy, uv)
 
     # get interpolated timeseries from hds_arr
-    hm_ts = np.zeros((obs_collection.shape[0], hds_arr.shape[0]))
+    hm_ts = np.nan * np.ones((obs_collection.shape[0], hds_arr.shape[0]))
+
+    if exclude_layers is None:
+        exclude_layers = []
 
     # loop over layers
     for m in range(nlay):
-        if m < exclude_layers:
+        if m in exclude_layers:
             continue
-        mask = obs_collection.modellayer.values == m
+        mask = obs_collection["modellayer"].values == m
         # loop over timesteps
         for t in range(hds_arr.shape[0]):
             ihds = hds_arr[t, m]
-            ihds[ihds < -999.] = np.nan
+            ihds[ihds <= -999.] = np.nan
             hm = util.interpolate(ihds, vtx, wts)
             hm_ts[mask, t] = hm[mask]
 
