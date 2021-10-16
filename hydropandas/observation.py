2e48c87d114a41855fdc8aa449a1c5e3330a83e6
OnnoEbbens <onnoebbens@gmail.com>
2021-10-16 11:07:24 +0200

    some codacy issues

--- hydropandas/observation.py
+++ hydropandas/observation.py
@@ -757,46 +757,98 @@ class MeteoObs(Obs):
             if True you get errors when no data is returned. The default is False.
 
         List of possible variables:
-            DDVEC = Vectorgemiddelde windrichting in graden (360=noord, 90=oost, 180=zuid, 270=west, 0=windstil/variabel). Zie http://www.knmi.nl/kennis-en-datacentrum/achtergrond/klimatologische-brochures-en-boeken / Vector mean wind direction in degrees (360=north, 90=east, 180=south, 270=west, 0=calm/variable)
-            FHVEC = Vectorgemiddelde windsnelheid (in 0.1 m/s). Zie http://www.knmi.nl/kennis-en-datacentrum/achtergrond/klimatologische-brochures-en-boeken / Vector mean windspeed (in 0.1 m/s)
-            FG    = Etmaalgemiddelde windsnelheid (in 0.1 m/s) / Daily mean windspeed (in 0.1 m/s)
-            FHX   = Hoogste uurgemiddelde windsnelheid (in 0.1 m/s) / Maximum hourly mean windspeed (in 0.1 m/s)
-            FHXH  = Uurvak waarin FHX is gemeten / Hourly division in which FHX was measured
-            FHN   = Laagste uurgemiddelde windsnelheid (in 0.1 m/s) / Minimum hourly mean windspeed (in 0.1 m/s)
-            FHNH  = Uurvak waarin FHN is gemeten / Hourly division in which FHN was measured
-            FXX   = Hoogste windstoot (in 0.1 m/s) / Maximum wind gust (in 0.1 m/s)
-            FXXH  = Uurvak waarin FXX is gemeten / Hourly division in which FXX was measured
-            TG    = Etmaalgemiddelde temperatuur (in 0.1 graden Celsius) / Daily mean temperature in (0.1 degrees Celsius)
-            TN    = Minimum temperatuur (in 0.1 graden Celsius) / Minimum temperature (in 0.1 degrees Celsius)
-            TNH   = Uurvak waarin TN is gemeten / Hourly division in which TN was measured
-            TX    = Maximum temperatuur (in 0.1 graden Celsius) / Maximum temperature (in 0.1 degrees Celsius)
-            TXH   = Uurvak waarin TX is gemeten / Hourly division in which TX was measured
-            T10N  = Minimum temperatuur op 10 cm hoogte (in 0.1 graden Celsius) / Minimum temperature at 10 cm above surface (in 0.1 degrees Celsius)
-            T10NH = 6-uurs tijdvak waarin T10N is gemeten / 6-hourly division in which T10N was measured; 6=0-6 UT, 12=6-12 UT, 18=12-18 UT, 24=18-24 UT
-            SQ    = Zonneschijnduur (in 0.1 uur) berekend uit de globale straling (-1 voor <0.05 uur) / Sunshine duration (in 0.1 hour) calculated from global radiation (-1 for <0.05 hour)
-            SP    = Percentage van de langst mogelijke zonneschijnduur / Percentage of maximum potential sunshine duration
+            DDVEC = Vectorgemiddelde windrichting in graden (360=noord,
+            90=oost, 180=zuid, 270=west, 0=windstil/variabel). Zie
+            http://www.knmi.nl/kennis-en-datacentrum/achtergrond/klimatologische-brochures-en-boeken
+            / Vector mean wind direction in degrees (360=north, 90=east,
+            180=south, 270=west, 0=calm/variable)
+            FHVEC = Vectorgemiddelde windsnelheid (in 0.1 m/s). Zie
+            http://www.knmi.nl/kennis-en-datacentrum/achtergrond/klimatologische-brochures-en-boeken
+            / Vector mean windspeed (in 0.1 m/s)
+            FG    = Etmaalgemiddelde windsnelheid (in 0.1 m/s) / Daily mean
+            windspeed (in 0.1 m/s)
+            FHX   = Hoogste uurgemiddelde windsnelheid (in 0.1 m/s) / Maximum
+            hourly mean windspeed (in 0.1 m/s)
+            FHXH  = Uurvak waarin FHX is gemeten / Hourly division in which
+            FHX was measured
+            FHN   = Laagste uurgemiddelde windsnelheid (in 0.1 m/s) / Minimum
+            hourly mean windspeed (in 0.1 m/s)
+            FHNH  = Uurvak waarin FHN is gemeten / Hourly division in which
+            FHN was measured
+            FXX   = Hoogste windstoot (in 0.1 m/s) / Maximum wind gust (in
+            0.1 m/s)
+            FXXH  = Uurvak waarin FXX is gemeten / Hourly division in which
+            FXX was measured
+            TG    = Etmaalgemiddelde temperatuur (in 0.1 graden Celsius) /
+            Daily mean temperature in (0.1 degrees Celsius)
+            TN    = Minimum temperatuur (in 0.1 graden Celsius) / Minimum
+            temperature (in 0.1 degrees Celsius)
+            TNH   = Uurvak waarin TN is gemeten / Hourly division in which TN
+            was measured
+            TX    = Maximum temperatuur (in 0.1 graden Celsius) / Maximum
+            temperature (in 0.1 degrees Celsius)
+            TXH   = Uurvak waarin TX is gemeten / Hourly division in which TX
+            was measured
+            T10N  = Minimum temperatuur op 10 cm hoogte (in 0.1 graden
+            Celsius) / Minimum temperature at 10 cm above surface (in 0.1
+            degrees Celsius)
+            T10NH = 6-uurs tijdvak waarin T10N is gemeten / 6-hourly division
+            in which T10N was measured; 6=0-6 UT, 12=6-12 UT, 18=12-18 UT,
+            24=18-24 UT
+            SQ    = Zonneschijnduur (in 0.1 uur) berekend uit de globale
+            straling (-1 voor <0.05 uur) / Sunshine duration (in 0.1 hour)
+            calculated from global radiation (-1 for <0.05 hour)
+            SP    = Percentage van de langst mogelijke zonneschijnduur /
+            Percentage of maximum potential sunshine duration
             Q     = Globale straling (in J/cm2) / Global radiation (in J/cm2)
-            DR    = Duur van de neerslag (in 0.1 uur) / Precipitation duration (in 0.1 hour)
-            RH    = Etmaalsom van de neerslag (in 0.1 mm) (-1 voor <0.05 mm) / Daily precipitation amount (in 0.1 mm) (-1 for <0.05 mm)
-            RHX   = Hoogste uursom van de neerslag (in 0.1 mm) (-1 voor <0.05 mm) / Maximum hourly precipitation amount (in 0.1 mm) (-1 for <0.05 mm)
-            RHXH  = Uurvak waarin RHX is gemeten / Hourly division in which RHX was measured
-            PG    = Etmaalgemiddelde luchtdruk herleid tot zeeniveau (in 0.1 hPa) berekend uit 24 uurwaarden / Daily mean sea level pressure (in 0.1 hPa) calculated from 24 hourly values
-            PX    = Hoogste uurwaarde van de luchtdruk herleid tot zeeniveau (in 0.1 hPa) / Maximum hourly sea level pressure (in 0.1 hPa)
-            PXH   = Uurvak waarin PX is gemeten / Hourly division in which PX was measured
-            PN    = Laagste uurwaarde van de luchtdruk herleid tot zeeniveau (in 0.1 hPa) / Minimum hourly sea level pressure (in 0.1 hPa)
-            PNH   = Uurvak waarin PN is gemeten / Hourly division in which PN was measured
-            VVN   = Minimum opgetreden zicht / Minimum visibility; 0: <100 m, 1:100-200 m, 2:200-300 m,..., 49:4900-5000 m, 50:5-6 km, 56:6-7 km, 57:7-8 km,..., 79:29-30 km, 80:30-35 km, 81:35-40 km,..., 89: >70 km)
-            VVNH  = Uurvak waarin VVN is gemeten / Hourly division in which VVN was measured
-            VVX   = Maximum opgetreden zicht / Maximum visibility; 0: <100 m, 1:100-200 m, 2:200-300 m,..., 49:4900-5000 m, 50:5-6 km, 56:6-7 km, 57:7-8 km,..., 79:29-30 km, 80:30-35 km, 81:35-40 km,..., 89: >70 km)
-            VVXH  = Uurvak waarin VVX is gemeten / Hourly division in which VVX was measured
-            NG    = Etmaalgemiddelde bewolking (bedekkingsgraad van de bovenlucht in achtsten, 9=bovenlucht onzichtbaar) / Mean daily cloud cover (in octants, 9=sky invisible)
-            UG    = Etmaalgemiddelde relatieve vochtigheid (in procenten) / Daily mean relative atmospheric humidity (in percents)
-            UX    = Maximale relatieve vochtigheid (in procenten) / Maximum relative atmospheric humidity (in percents)
-            UXH   = Uurvak waarin UX is gemeten / Hourly division in which UX was measured
-            UN    = Minimale relatieve vochtigheid (in procenten) / Minimum relative atmospheric humidity (in percents)
-            UNH   = Uurvak waarin UN is gemeten / Hourly division in which UN was measured
-            EV24  = Referentiegewasverdamping (Makkink) (in 0.1 mm) / Potential evapotranspiration (Makkink) (in 0.1 mm)
-
+            DR    = Duur van de neerslag (in 0.1 uur) / Precipitation duration
+            (in 0.1 hour)
+            RH    = Etmaalsom van de neerslag (in 0.1 mm) (-1 voor <0.05 mm) /
+            Daily precipitation amount (in 0.1 mm) (-1 for <0.05 mm)
+            RHX   = Hoogste uursom van de neerslag (in 0.1 mm) (-1 voor <0.05
+            mm) / Maximum hourly precipitation amount (in 0.1 mm) (-1 for 
+            <0.05 mm)
+            RHXH  = Uurvak waarin RHX is gemeten / Hourly division in which
+            RHX was measured
+            PG    = Etmaalgemiddelde luchtdruk herleid tot zeeniveau (in 0.1
+            hPa) berekend uit 24 uurwaarden / Daily mean sea level pressure
+            (in 0.1 hPa) calculated from 24 hourly values
+            PX    = Hoogste uurwaarde van de luchtdruk herleid tot zeeniveau
+            (in 0.1 hPa) / Maximum hourly sea level pressure (in 0.1 hPa)
+            PXH   = Uurvak waarin PX is gemeten / Hourly division in which PX
+            was measured
+            PN    = Laagste uurwaarde van de luchtdruk herleid tot zeeniveau
+            (in 0.1 hPa) / Minimum hourly sea level pressure (in 0.1 hPa)
+            PNH   = Uurvak waarin PN is gemeten / Hourly division in which PN
+            was measured
+            VVN   = Minimum opgetreden zicht / Minimum visibility; 0: <100 m,
+            1:100-200 m, 2:200-300 m,..., 49:4900-5000 m, 50:5-6 km,
+            56:6-7 km, 57:7-8 km,..., 79:29-30 km, 80:30-35 km, 81:35-40 km,
+            ..., 89: >70 km)
+            VVNH  = Uurvak waarin VVN is gemeten / Hourly division in which
+            VVN was measured
+            VVX   = Maximum opgetreden zicht / Maximum visibility; 0: <100 m,
+            1:100-200 m, 2:200-300 m,..., 49:4900-5000 m, 50:5-6 km,
+            56:6-7 km, 57:7-8 km,..., 79:29-30 km, 80:30-35 km, 81:35-40 km,
+            ..., 89: >70 km)
+            VVXH  = Uurvak waarin VVX is gemeten / Hourly division in which
+            VVX was measured
+            NG    = Etmaalgemiddelde bewolking (bedekkingsgraad van de
+            bovenlucht in achtsten, 9=bovenlucht onzichtbaar) / Mean daily
+            cloud cover (in octants, 9=sky invisible)
+            UG    = Etmaalgemiddelde relatieve vochtigheid (in procenten) /
+            Daily mean relative atmospheric humidity (in percents)
+            UX    = Maximale relatieve vochtigheid (in procenten) / Maximum
+            relative atmospheric humidity (in percents)
+            UXH   = Uurvak waarin UX is gemeten / Hourly division in which UX
+            was measured
+            UN    = Minimale relatieve vochtigheid (in procenten) / Minimum
+            relative atmospheric humidity (in percents)
+            UNH   = Uurvak waarin UN is gemeten / Hourly division in which UN
+            was measured
+            EV24  = Referentiegewasverdamping (Makkink) (in 0.1 mm) /
+            Potential evapotranspiration (Makkink) (in 0.1 mm)
+            
         Returns
         -------
         MeteoObs object with meteorological observations
@@ -978,12 +1030,12 @@ class PrecipitationObs(MeteoObs):
         """Get a PrecipitationObs timeseries from the KNMI precipitation. The
         precipitation is the Daily precipitation amount (in 0.1 mm) (-1 for
         <0.05 mm).
-        
+
         There are 3 different ways to obtain precipitation data from the knmi:
             1. Daily data from precipitation (neerslag) stations
             2. Daily data from meteo stations
             3. Hourly data from meteo stations
-        
+
         1. If you want to get data from a neerslagstation stn should be the 
         station number with '_neerslag_station' at the end e.g. for De Bilt: 
             stn = '550_neerslag_station'.
