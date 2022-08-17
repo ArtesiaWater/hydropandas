43d32fdc1ff99223382cdd63fcc22fe5b4bc09f2
OnnoEbbens <onnoebbens@gmail.com>
2022-08-17 16:58:25 +0200

    make flopy and shapely optional dependencies

--- hydropandas/extensions/gwobs.py
+++ hydropandas/extensions/gwobs.py
@@ -1,11 +1,9 @@
 import logging
 
-import flopy
 import numpy as np
 import pandas as pd
 
 from . import accessor
-from shapely.geometry import Point
 
 logger = logging.getLogger(__name__)
 
@@ -242,6 +240,8 @@ def get_zvec(x, y, gwf=None, ds=None):
     zvec : list
         list of vertical layer boundaries. length is nlay + 1.
     """
+    import flopy
+    from shapely.geometry import Point
 
     if gwf and not ds:
         ix = flopy.utils.GridIntersect(gwf.modelgrid)
