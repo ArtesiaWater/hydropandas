9f7042a5a5f83c23c2382ea77cd26a8524c9758a
OnnoEbbens <onnoebbens@gmail.com>
2022-08-17 17:13:27 +0200

    update versions

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     platforms="Windows, Mac OS-X",
-    install_requires=["scipy", "pandas>=1.0", "tqdm", "requests", "zeep"],
+    install_requires=["scipy", "pandas", "tqdm", "requests", "zeep"],
     packages=find_packages(exclude=[]),
     package_data={"hydropandas": ["data/*.json"]},
     include_package_data=True,
