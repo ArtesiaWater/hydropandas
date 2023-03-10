8791de44ae3ac11cd70931385090ea03850f0d03
Florisklooster <62564920+Florisklooster@users.noreply.github.com>
2023-03-10 09:30:55 +0100

    Prevent runtime error, with mulitple bro id's
    In some 'grondwatermonitoringsputten' two bro id's are returned, one with a quality regime of 'IMBRO/A' and one with 'IMBRO'.
    Right now this gives a runtime error. In this proposed change I have put the default on 'IMBRO/A', if something like the above would be implented in the code, than of course the quality_regime parameter should be in other functions as well.
    
    By the way, I  really appreciate the effort of implementing this BRO extension

--- hydropandas/io/io_bro.py
+++ hydropandas/io/io_bro.py
@@ -148,7 +148,7 @@ def get_bro_groundwater(bro_id, tube_nr=None, only_metadata=False, **kwargs):
         return measurements_from_gld(gld_id, **kwargs)
 
 
-def get_gld_id_from_gmw(bro_id, tube_nr):
+def get_gld_id_from_gmw(bro_id, tube_nr, quality_regime='IMBRO/A'):
     """get bro_id of a grondwterstandendossier (gld) from a bro_id of a
     grondwatermonitoringsput (gmw).
 
@@ -158,7 +158,9 @@ def get_gld_id_from_gmw(bro_id, tube_nr):
         starts with 'GLD' or 'GMW' e.g. 'GMW000000036287'.
     tube_nr : int
         tube number.
-
+    quality_regime : str
+        either choose 'IMBRO/A' or 'IMBRO'.
+        
     Raises
     ------
     ValueError
@@ -198,6 +200,21 @@ def get_gld_id_from_gmw(bro_id, tube_nr):
                     f"no groundwater level dossier for {bro_id} and tube number {tube_nr}"
                 )
                 return None
+            elif len(tube["gldReferences"]) == 2:
+                url2 = tube["gldReferences"][0]["url"]
+                req2 = requests.get(url2)
+                ns = {
+                    "ns11": "http://www.broservices.nl/xsd/dsgld/1.0",
+                    "brocom" : "http://www.broservices.nl/xsd/brocommon/3.0"
+                }
+                tree = xml.etree.ElementTree.fromstring(req2.text)
+                glds = tree.findall(".//ns11:GLD_O", ns)
+                gld = glds[0]
+                qualityRegime =  gld.find("brocom:qualityRegime", ns).text
+                if qualityRegime == quality_regime:
+                    return tube["gldReferences"][0]["broId"]
+                else:
+                    return tube["gldReferences"][1]["broId"]
             else:
                 raise RuntimeError("unexpected number of gld references")
 
