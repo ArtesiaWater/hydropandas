7ee2d611fe262ed28eeb027d56404cad2ba2fef5
Martin Vonk <vonk.mart@gmail.com>
2023-03-15 15:43:37 +0100

    fix test

--- tests/test_011_io_bro.py
+++ tests/test_011_io_bro.py
@@ -65,9 +65,9 @@ def test_get_gld_id_from_gmw():
 
 def test_get_gld_id_from_gmw_quality_regime():
     bro_id = "GMW000000063853"  # bro id with two gld's with a different quality regime, both gld's have no measurements 13-3-2023
-    gld1 = io_bro.get_gld_id_from_gmw(bro_id, tube_nr=1, quality_regime="IMBRO/A")
+    gld1 = bro.get_gld_id_from_gmw(bro_id, tube_nr=1, quality_regime="IMBRO/A")
 
-    gld2 = io_bro.get_gld_id_from_gmw(bro_id, tube_nr=1, quality_regime="IMBRO")
+    gld2 = bro.get_gld_id_from_gmw(bro_id, tube_nr=1, quality_regime="IMBRO")
     assert (
         gld1 != gld2
     ), "different quality regimes should return different gld id's for this gmw id"
