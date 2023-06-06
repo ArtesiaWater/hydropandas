919e8336a24c98a1737364b3559c1c85f0938284
Martin Vonk <vonk.mart@gmail.com>
2023-06-06 15:24:40 +0200

    clean up test wow

--- tests/test_012_wow.py
+++ tests/test_012_wow.py
@@ -1,14 +1,8 @@
-import logging
-
 import pandas as pd
 
 import hydropandas as hpd
 from hydropandas.io import wow
 
-logging.basicConfig(level=logging.DEBUG)
-%load_ext autoreload
-%autoreload 2
-#%%
 
 def test_wow_strftime() -> None:
     timestamp = pd.Timestamp(year=2010, month=10, day=2, hour=10, minute=34, second=0)
@@ -23,17 +17,16 @@ def test_get_wow_stn() -> None:
     obs = hpd.MeteoObs.from_wow(
         stn=stn, meteo_var="temperature", startdate=startdate, enddate=enddate
     )
+    assert obs.name == "Macquarie Island"
+
 
 def test_get_wow_nearest() -> None:
     lat = -35.4184
     lon = 149.0937
     xy = [lon, lat]
-
-        # # stn = "423216079"  # Macquarie Island
     startdate = pd.Timestamp(year=2023, month=6, day=4, hour=1, minute=34, second=0)
     enddate = pd.Timestamp(year=2023, month=6, day=5, hour=23, minute=54, second=0)
     obs = hpd.PrecipitationObs.from_wow_nearest_xy(
         xy=xy, startdate=startdate, enddate=enddate
     )
-    # return obs
     assert obs.name == "Hillanhome"
