a10c9e611785f0b479bb07b3293772c6658aca76
OnnoEbbens <onnoebbens@gmail.com>
2024-03-12 14:17:14 +0100

    fix for #102

--- tests/test_003_calculations.py
+++ tests/test_003_calculations.py
@@ -2,7 +2,7 @@ import test_001_to_from as ttf
 
 
 def test_within_extent():
-    dino_gw = ttf.test_obscollection_dinozip_gw()
+    dino_gw = ttf.obscollection_dinozip_gw()
     extent = [210350, 213300, 473300, 474000]
     dino_gw.geo.within_extent(extent, inplace=True)
     assert dino_gw.shape[0] == 4
@@ -12,32 +12,32 @@ def test_within_extent():
 
 
 def test_obscollection_consecutive_obs_years():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.consecutive_obs_years()
 
 
 def test_obscollection_get_number_of_obs():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.get_no_of_observations()
 
 
 def test_obscollection_get_first_last_obs_date():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.get_first_last_obs_date()
 
 
 def test_obscollection_get_seasonal_stats():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.get_seasonal_stat(stat="mean")
 
 
 def test_obscollection_get_min():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.get_min()
 
 
 def test_obscollection_get_max():
-    gw = ttf.test_obscollection_dinozip_gw_keep_all_obs()
+    gw = ttf.obscollection_dinozip_gw_keep_all_obs()
     gw.stats.get_max()
 
 
@@ -47,8 +47,8 @@ def test_obscollection_get_max():
 def test_get_nearest_point():
     # check two of the same observation collections
     # every point must find itself as the nearest point
-    dino_gw = ttf.test_obscollection_dinozip_gw()
-    fl = ttf.test_obscollection_dinozip_wl()
+    dino_gw = ttf.obscollection_dinozip_gw()
+    fl = ttf.obscollection_dinozip_wl()
     dino_gw[["nearest point", "distance nearest point"]] = (
         dino_gw.geo.get_nearest_point(fl)
     )
@@ -60,7 +60,7 @@ def test_get_nearest_polygon():
 
     # check two of the same observation collections
     # every point must find itself as the nearest point
-    dino_gw = ttf.test_obscollection_dinozip_gw()
+    dino_gw = ttf.obscollection_dinozip_gw()
     extent = dino_gw.geo.get_extent()
     polygon1 = Polygon(
         (
@@ -93,7 +93,7 @@ def test_get_ground_level_oc():
     try:
         from art_tools import hpd_extension  # noqa: F401
 
-        gw = ttf.test_obscollection_fews_lowmemory()
+        gw = ttf.obscollection_fews_lowmemory()
         gw.art.geo_get_ground_level()
         return
     except ModuleNotFoundError as e:
@@ -104,7 +104,7 @@ def test_get_ground_level_gwobs():
     try:
         from art_tools import hpd_extension  # noqa: F401
 
-        gw = ttf.test_observation_gw()
+        gw = ttf.observation_gw()
         gw.art.geo_get_ground_level()
         return
     except ModuleNotFoundError as e:
