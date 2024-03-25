4ca094bb51d80eb61ee6601c61f8a20e94a1277a
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-03-25 17:26:13 +0100

    standardize show_version

--- hydropandas/version.py
+++ hydropandas/version.py
@@ -1,1 +1,17 @@
+from importlib import metadata
+from sys import version as os_version
+
 __version__ = "0.11.2b"
+
+
+def show_versions():
+    """Method to print the versions of dependencies."""
+    msg = (
+        f"Python version      : {os_version}\n"
+        f"Numpy version       : {metadata.version('numpy')}\n"
+        f"Scipy version       : {metadata.version('scipy')}\n"
+        f"Pandas version      : {metadata.version('pandas')}\n"
+        f"Matplotlib version  : {metadata.version('matplotlib')}\n\n"
+        f"Hydropandas version : {__version__}"
+    )
+    return print(msg)
