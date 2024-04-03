ac11c38ac6998ac36fd06ee821ec5e5b534559ac
OnnoEbbens <onnoebbens@gmail.com>
2024-04-03 16:00:12 +0200

    fix test

--- tests/test_011_bro.py
+++ tests/test_011_bro.py
@@ -52,9 +52,9 @@ def test_get_gld_ids_from_gmw():
     bro.get_gld_ids_from_gmw(bro_id, tube_nr=1)
 
 
-def test_obs_list_from_extent():
+def test_groundwater_from_extent():
     # extent = (102395, 103121, 434331, 434750)
     extent = [116500, 120000, 439000, 442000]
-    bro.get_obs_list_from_extent(
+    bro.get_groundwater_from_extent(
         extent, hpd.GroundwaterObs, tmin=None, tmax=None, epsg=28992, only_metadata=True
     )
