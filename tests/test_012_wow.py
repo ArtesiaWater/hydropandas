e05943afe5bb691dbac62b99e039309e0b9216a4
Martin Vonk <vonk.mart@gmail.com>
2024-09-18 09:50:46 +0200

    skip test with pytest

--- tests/test_012_wow.py
+++ tests/test_012_wow.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-# import hydropandas as hpd
+import pytest
+import hydropandas as hpd
 from hydropandas.io import wow
 
 
@@ -10,19 +11,21 @@ def test_wow_strftime() -> None:
     assert timestampstr == "2010-10-02T10%3A30%3A00"
 
 
-# def test_get_wow_stn() -> None:
-#     stn = "423216079"  # Macquarie Island
-#     start = pd.Timestamp(year=2023, month=6, day=4, hour=1, minute=34, second=0)
-#     end = pd.Timestamp(year=2023, month=6, day=5, hour=23, minute=54, second=0)
-#     obs = hpd.MeteoObs.from_wow(stn=stn, meteo_var="temperature", start=start, end=end)
-#     assert obs.name == "Macquarie Island"
+@pytest.mark.skip
+def test_get_wow_stn() -> None:
+    stn = "423216079"  # Macquarie Island
+    start = pd.Timestamp(year=2023, month=6, day=4, hour=1, minute=34, second=0)
+    end = pd.Timestamp(year=2023, month=6, day=5, hour=23, minute=54, second=0)
+    obs = hpd.MeteoObs.from_wow(stn=stn, meteo_var="temperature", start=start, end=end)
+    assert obs.name == "Macquarie Island"
 
 
-# def test_get_wow_nearest() -> None:
-#     lat = -35.4184
-#     lon = 149.0937
-#     xy = [lon, lat]
-#     start = pd.Timestamp(year=2023, month=6, day=4, hour=1, minute=34, second=0)
-#     end = pd.Timestamp(year=2023, month=6, day=5, hour=23, minute=54, second=0)
-#     obs = hpd.PrecipitationObs.from_wow(xy=xy, start=start, end=end)
-#     assert obs.name == "Hillanhome"
+@pytest.mark.skip
+def test_get_wow_nearest() -> None:
+    lat = -35.4184
+    lon = 149.0937
+    xy = [lon, lat]
+    start = pd.Timestamp(year=2023, month=6, day=4, hour=1, minute=34, second=0)
+    end = pd.Timestamp(year=2023, month=6, day=5, hour=23, minute=54, second=0)
+    obs = hpd.PrecipitationObs.from_wow(xy=xy, start=start, end=end)
+    assert obs.name == "Hillanhome"
