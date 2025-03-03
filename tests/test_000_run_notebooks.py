498868848c039c4d701c23a8b1d731248269ec24
OnnoEbbens <onnoebbens@gmail.com>
2025-03-03 15:03:36 +0100

    Add introduction to Obs and ObsCollection

--- tests/test_000_run_notebooks.py
+++ tests/test_000_run_notebooks.py
@@ -21,6 +21,11 @@ def _run_notebook(nbdir, fname):
 
 
 @pytest.mark.notebooks
+def test_run_notebook_00_hydropandas_objects():
+    _run_notebook(nbdir, "00_hydropandas_objects.ipynb")
+
+
+@pytest.mark.notebooks
 def test_run_notebook_01_groundwater_observations():
     _run_notebook(nbdir, "01_groundwater_observations.ipynb")
 
@@ -44,3 +49,12 @@ def test_run_notebook_03_hydropandas_and_pastas():
 @pytest.mark.notebooks
 def test_run_notebook_05_bronhouderportaal_bro():
     _run_notebook(nbdir, "05_bronhouderportaal_bro.ipynb")
+
+@pytest.mark.notebooks
+def test_run_notebook_06_lizard():
+    _run_notebook(nbdir, "06_lizard.ipynb")
+
+
+@pytest.mark.notebooks
+def test_run_notebook_07_fews():
+    _run_notebook(nbdir, "07_fews.ipynb")
