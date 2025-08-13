4f837d4c9f4ba97f598c20245f7969882c8132cd
Mattijs Borst <m.borst@cwgi.nl>
2025-08-13 16:22:12 +0200

    add tests for Rotterdam measurements

--- tests/test_013_lizard.py
+++ tests/test_013_lizard.py
@@ -2,7 +2,7 @@ import pytest
 
 import hydropandas as hpd
 
-
+# Original tests when only Vitens was supported
 def test_single_observation():
     code = "27BP0003"
     o = hpd.GroundwaterObs.from_lizard(code)
@@ -37,3 +37,29 @@ def test_complex_well():
 
 def test_combine():
     hpd.GroundwaterObs.from_lizard("39F-0736", tube_nr=1, type_timeseries="combine")
+
+# Additional tests for use with the 'Rotterdam' data
+
+# @OnnoEbbens: Please add here the reference to the API key in the Github secrets
+api_key_rotterdam = ""
+auth = ("__key__", api_key_rotterdam)
+
+def test_single_observation_rotterdam():
+    code = "GMW000000036819"
+    o = hpd.GroundwaterObs.from_lizard(code, organisation="rotterdam", auth=auth)
+    assert o.tube_nr == 1
+
+
+def test_extent_rotterdam():
+    extent = [68_500, 69_500, 443_500, 444_500]
+    oc = hpd.read_lizard(extent, organisation="rotterdam", auth=auth)
+    assert not oc.empty
+
+
+@pytest.mark.slow
+def test_codes_rotterdam():
+    oc = hpd.read_lizard(
+        codes=["GMW000000036819", "GMW000000037933"], type_timeseries="merge", organisation="rotterdam", auth=auth
+    )
+    assert not oc.empty
+
