d00ba0db0d5a22c96ac4b4231ef8617e9efa7e57
TomHottentot <tom.hottentot@rhdhv.com>
2026-03-04 14:53:59 +0100

    Removed import typing.Dict in observation.py
    This was an error in Codacy

--- hydropandas/observation.py
+++ hydropandas/observation.py
@@ -20,7 +20,7 @@ import numbers
 import os
 import warnings
 from io import StringIO, TextIOWrapper
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 import numpy as np
 import pandas as pd
