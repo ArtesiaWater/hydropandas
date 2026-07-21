4e25de23a5db81d402eaacc9c571f30f9f5df708
martinvonk <vonk.mart@gmail.com>
2026-07-21 15:25:26 +0200

    fix update_knmi_stations script

--- hydropandas/data/update_knmi_stations.py
+++ hydropandas/data/update_knmi_stations.py
@@ -6,69 +6,114 @@ import requests
 
 from hydropandas.io import knmi
 
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 class EmptyDataFrameError(Exception):
     pass
 
+#%%
+# read tmin tmax from knmi website
+meteo_tminmax_nederland = (
+    pd.read_html("https://www.knmi.nl/nederland-nu/klimatologie/daggegevens")[0]
+    .dropna(how="any")
+    .convert_dtypes()
+    .set_index("Nummer")
+)
+meteo_tminmax_noordzee = pd.read_html(
+    "https://www.knmi.nl/nederland-nu/klimatologie/daggegevens_Noordzee"
+)[0].dropna(how="any").convert_dtypes().set_index("Nummer")
+meteo_tminmax_knmi = pd.concat([meteo_tminmax_nederland, meteo_tminmax_noordzee]).sort_index()
+meteo_tminmax_knmi["tmin"] = [
+    pd.to_datetime(x, format="%Y%m%d").strftime("%Y-%m-%d")
+    for x in meteo_tminmax_knmi["Vanaf"]
+]
+meteo_tminmax_knmi["tmax"] = [
+    pd.to_datetime(x, format="%Y%m%d") if x != "gisteren" else pd.Timestamp.today() for x in meteo_tminmax_knmi["Tot en met"]
+]
+meteo_tminmax_knmi["tmax"] = [
+    None
+    if (pd.Timestamp.today() - x) < pd.Timedelta(days=365)
+    else x.strftime("%Y-%m-%d")
+    for x in meteo_tminmax_knmi["tmax"]
+]
 
 # %%
-# meteo stations
+# read hydropandas knmi meteo stations bookkeeping
 meteo_df = pd.read_json("knmi_meteostation.json")
-meteo_dft = meteo_df.copy()
+assert meteo_tminmax_knmi.index.difference(meteo_df.index).empty, "KNMI meteo station index does not match KNMI tmin/tmax index"
+#%%
+# fill meteo_df with tmin, tmax of meteo_tminmax_knmi
+meteo_df.loc[meteo_tminmax_knmi.index, "tmin"] = meteo_tminmax_knmi["tmin"]
+meteo_df.loc[meteo_tminmax_knmi.index, "tmax"] = meteo_tminmax_knmi["tmax"]
+
+#%%
+# some extra metadata file from knmi data platform, but not used in hydropandas
+# because our locations are more accurate
+# https://dataplatform.knmi.nl/dataset/waarneemstations-csv-1-0
+# meteo_meta = pd.read_csv("csv_knmi_nl_20260615.csv", index_col=0)
+
+#%%
+# loop through all KNMI meteo stations and check if data is available via url and api
+meteo_tminmax_url = pd.DataFrame(index=meteo_tminmax_knmi.index, columns=["tmin", "tmax"])
+meteo_tminmax_api = pd.DataFrame(index=meteo_tminmax_knmi.index, columns=["tmin", "tmax"])
 
 didx = [pd.Timestamp.today().normalize() - pd.Timedelta(days=1)] + list(
     reversed(
         pd.date_range(pd.Timestamp("1900-01-01"), pd.Timestamp.today(), freq="10YS")
     )
 )
-
-
-for stn in meteo_dft.index:
+for stn in meteo_df.index:
+    logger.info(f"Checking KNMI meteo station {stn}")
     df_daily, _ = knmi.get_knmi_daily_meteo_url(stn)
     vars_available_daily = ~df_daily.isna().all().drop(index=["STN"])
-    meteo_dft.loc[stn, vars_available_daily.index] = vars_available_daily.values
+    meteo_df.loc[stn, vars_available_daily.index] = vars_available_daily.values
+    meteo_tminmax_url.loc[stn, "tmin"] = df_daily.index[0]
+    meteo_tminmax_url.loc[stn, "tmax"] = df_daily.index[-1]
 
     for end, start in zip(didx[:-1], didx[1:]):
         try:
-            df_hourly, _ = knmi.get_knmi_hourly_meteo_api(stn, start=start, end=end)
+            df_hourly, _ = knmi.get_hourly_meteo_api(stn, start=start, end=end)
             if df_hourly.empty:
                 raise EmptyDataFrameError
             vars_available_hourly = ~df_hourly.isna().all().drop(index=["STN"])
             vah = vars_available_hourly[vars_available_hourly]
-            meteo_dft.loc[stn, vah.index] = vah.values
+            meteo_df.loc[stn, vah.index] = vah.values
         except (
             pd.errors.EmptyDataError,
             requests.ConnectionError,
+            requests.HTTPError,
             EmptyDataFrameError,
-        ):
-            print(f"Geen data {stn}, {start}, {end}")
+        ) as e:
+            logger.error(f"Geen data {stn}, {start}, {end}, {e}")
 
-meteo_dft = meteo_dft.fillna(False).drop(columns=[""])
+    try:
+        df_daily, _ = knmi.get_daily_meteo_api(stn)
+        meteo_df.loc[stn, "api_available"] = True
+        meteo_tminmax_api.loc[stn, "tmin"] = df_daily.index[0]
+        meteo_tminmax_api.loc[stn, "tmax"] = df_daily.index[-1]
 
-# add tmin, tmax
-meteo_tminmax = (
-    pd.read_html("https://www.knmi.nl/nederland-nu/klimatologie/daggegevens")[0]
-    .dropna(how="any")
-    .convert_dtypes()
-    .set_index("Nummer")
-)
-meteo_tminmax["tmin"] = [
-    pd.to_datetime(x, format="%Y%m%d").strftime("%Y-%m-%d")
-    for x in meteo_tminmax["Vanaf"]
-]
-meteo_tminmax["tmax"] = [
-    pd.to_datetime(x, format="%Y%m%d") for x in meteo_tminmax["Tot en met"]
-]
-meteo_tminmax["tmax"] = [
-    None
-    if (pd.Timestamp.today() - x) < pd.Timedelta(days=365)
-    else x.strftime("%Y-%m-%d")
-    for x in meteo_tminmax["tmax"]
-]
-meteo_dft.loc[meteo_tminmax.index, "tmin"] = meteo_tminmax["tmin"].values
-meteo_dft.loc[meteo_tminmax.index, "tmax"] = meteo_tminmax["tmax"].values
+    except requests.HTTPError as e:
+        meteo_df.loc[stn, "api_available"] = False
+        logger.error(f"Geen data {stn}, {e}. Setting api_available to False")
 
+#%%
+# save meteo data variables to json
+meteo_dft = meteo_df.fillna(False).drop(columns=[""])
 meteo_dft.to_json("knmi_meteostation.json")
+
+#%%
+# compare tminmax api, url and knmi website
+meteo_tminmax_compare = pd.concat(
+    [
+        meteo_tminmax_knmi[["tmin", "tmax"]].rename(columns={"tmin": "tmin_knmi", "tmax": "tmax_knmi"}),
+        meteo_tminmax_url.rename(columns={"tmin": "tmin_url", "tmax": "tmax_url"}),
+        meteo_tminmax_api.rename(columns={"tmin": "tmin_api", "tmax": "tmax_api"}),
+    ],
+    axis=1,
+)
+
 # %%
 # neerslag stations
 prec_df = pd.read_json("knmi_neerslagstation.json")
